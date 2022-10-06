958dd707c2eead203b897e970f5dde7d1f906357
MiNHAZ-33 <miminhaz33@gmail.com>
2022-10-06 20:22:52 +0600

    generated token by jsonwebtoken package

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler';
 
 import User from "../model/userModel.js";
+import generateToken from '../utils/generateTokens.js';
 
 const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
@@ -12,7 +13,7 @@ const authUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     } else {
         res.status(401);
@@ -20,4 +21,4 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
-export {authUser}
+export { authUser }
