bb012438e19dcea58c73c96f60488bfd365cc290
MiNHAZ-33 <miminhaz33@gmail.com>
2022-10-06 19:21:11 +0600

    Added product controller in backend

--- BackEnd/routes/productRoutes.js
+++ BackEnd/routes/productRoutes.js
@@ -1,24 +1,10 @@
 import express from "express";
-import Product from "../model/productModel.js";
-import asyncHandler from 'express-async-handler';
+import { getPoductById, getProducts } from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products)
-}))
+router.route('/').get(getProducts);
 
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-        res.json(product);
-    } else {
-        
-        res.status(404);
-        throw new Error('Product not found')
-    }
-}))
+router.route('/:id').get(getPoductById);
 
 export default router;
--- FrontEnd/src/components/Header.js
+++ FrontEnd/src/components/Header.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 function Header() {
+
+    const cart = useSelector(state => state.cart);
+
+    const { cartItems } = cart;
+
     return (
         <div className="container navbar bg-base-100">
             <div className="flex-auto lg:pl-20">
@@ -13,7 +19,7 @@ function Header() {
                     <label tabIndex={0} className="btn btn-ghost btn-circle">
                         <div className="indicator">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                            <span className="badge badge-sm indicator-item">8</span>
+                            <span className="badge badge-sm indicator-item">{cartItems.reduce((acc, item)=> acc + item.qty, 0)}</span>
                         </div>
                     </label>
                     </Link>
