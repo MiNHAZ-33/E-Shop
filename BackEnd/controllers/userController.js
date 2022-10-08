3caa787a1cf831af64266f23f70491f91f5e0e4b
MiNHAZ-33 <miminhaz33@gmail.com>
2022-10-08 19:09:51 +0600

    completed login registration and token generation in backend

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -11,7 +11,8 @@ const authUser = asyncHandler(async (req, res) => {
     if (user && (await user.matchPassword(password))) {
         res.json({
             _id: user.id,
-            name: user.email,
+            name: user.name,
+            email: user.email,
             isAdmin: user.isAdmin,
             token: generateToken(user._id)
         })
@@ -21,4 +22,50 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser }
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+    if (user) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(401);
+        throw new Error('User not found')
+    }
+})
+
+
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password } = req.body;
+
+    const userExist = await User.findOne({ email });
+
+    if (userExist) {
+        res.status(400);
+        throw new Error('User already exist')
+    } 
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400);
+        throw new Error('User not found');
+    }
+})
+
+export { authUser, getUserProfile, registerUser }
