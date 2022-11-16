b4afe48df85b176db66c1cd0fced181230eace25
MiNHAZ-33 <miminhaz33@gmail.com>
2022-11-16 23:07:40 +0600

    backend ready for review

--- BackEnd/routes/productRoutes.js
+++ BackEnd/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createProduct, deletePoduct, getPoductById, getProducts, updateProduct } from "../controllers/productController.js";
+import { createProduct, createProductReview, deletePoduct, getPoductById, getProducts, updateProduct } from "../controllers/productController.js";
 import {protect, admin} from '../middlewire/authMiddleware.js'
 
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 
 router.route('/:id').get(getPoductById).delete(protect, admin, deletePoduct ).put(protect, admin, updateProduct);
 
