6e27b0113494610ae7545c8bc1a9c17f5d47980c
MiNHAZ-33 <miminhaz33@gmail.com>
2022-11-16 17:07:27 +0600

    backend ready for create and edit product

--- BackEnd/routes/productRoutes.js
+++ BackEnd/routes/productRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { deletePoduct, getPoductById, getProducts } from "../controllers/productController.js";
+import { createProduct, deletePoduct, getPoductById, getProducts, updateProduct } from "../controllers/productController.js";
 import {protect, admin} from '../middlewire/authMiddleware.js'
 
 const router = express.Router();
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 
-router.route('/:id').get(getPoductById).delete(protect, admin, deletePoduct );
+router.route('/:id').get(getPoductById).delete(protect, admin, deletePoduct ).put(protect, admin, updateProduct);
 
 export default router;
